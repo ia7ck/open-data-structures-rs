d22823ad622d33d284026cbaaf1572cb395eb5f8
ia7ck <23146842+ia7ck@users.noreply.github.com>
2022-03-13 19:19:22 +0900

    add benchmark: DLList, Vec, SkipListList

---
+++ skiplist-list/examples/bench.rs
@@ -0,0 +1,46 @@
+use std::time::Instant;
+
+use rand::{rngs::SmallRng, Rng, SeedableRng};
+
+use dllist::DLList;
+use interface::List;
+use skiplist_list::SkipListList;
+
+fn main() {
+    let mut rng = SmallRng::seed_from_u64(122333);
+    let n = 500_00;
+    let mut a = Vec::new();
+    for i in 0..n {
+        a.push(rng.gen_range(0..=i));
+    }
+
+    let mut dl_list = DLList::new();
+    let now = Instant::now();
+    for &a in &a {
+        dl_list.add(a, a);
+    }
+    for &a in a.iter().rev() {
+        dl_list.remove(a.saturating_sub(1));
+    }
+    println!("DLList {} ms", now.elapsed().as_millis());
+
+    let mut v = Vec::new();
+    let now = Instant::now();
+    for &a in &a {
+        v.insert(a, a);
+    }
+    for &a in a.iter().rev() {
+        v.remove(a.saturating_sub(1));
+    }
+    println!("std::vec::Vec {} ms", now.elapsed().as_millis());
+
+    let mut skip_list = SkipListList::new();
+    let now = Instant::now();
+    for &a in &a {
+        skip_list.add(a, a);
+    }
+    for &a in a.iter().rev() {
+        skip_list.remove(a.saturating_sub(1));
+    }
+    println!("SkipListList {} ms", now.elapsed().as_millis());
+}
