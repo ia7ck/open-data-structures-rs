be32e04a2750f103f03cfbd1ac3bf881f3188fdf
ia7ck <23146842+ia7ck@users.noreply.github.com>
2022-04-11 09:25:13 +0900

    implement SSet by ScapegoatTree

--- benchmark/src/bin/sset.rs
+++ benchmark/src/bin/sset.rs
@@ -1,9 +1,10 @@
 use std::collections::BTreeSet;
 use std::time::{Duration, Instant};
 
-use rand::{rngs::SmallRng, seq::SliceRandom, Rng, SeedableRng};
+use rand::{rngs::SmallRng, Rng, SeedableRng};
 
 use interface::SSet;
+use scapegoat_tree::ScapegoatTree;
 use skiplist_sset::SkipListSSet;
 use treap::Treap;
 
@@ -37,30 +38,49 @@ fn add_remove<T>(mut set: impl SSet<T>, a: Vec<T>, b: Vec<T>) -> Duration {
     now.elapsed()
 }
 
-fn main() {
-    let mut rng = SmallRng::seed_from_u64(1223334);
-    let n = 200_000;
-    let mut a = vec![0_i64; n];
-    rng.fill(&mut a[..]);
-    // b: 半分が a の要素、半分がランダム
-    let mut b: Vec<i64> = a
-        .iter()
-        .copied()
-        .take(n / 2)
-        .chain(std::iter::repeat_with(|| rng.gen()).take(n / 2))
-        .collect();
-    b.shuffle(&mut rng);
-
+fn run<T>(label: &str, a: Vec<T>, b: Vec<T>)
+where
+    T: Clone + Ord,
+{
     let elapsed = add_remove(MyBTreeSet(BTreeSet::new()), a.clone(), b.clone());
-    println!("std::collections::BTreeSet {} ms", elapsed.as_millis());
+    println!(
+        "[{}] std::collections::BTreeSet {} ms",
+        label,
+        elapsed.as_millis()
+    );
 
     let elapsed = add_remove(SkipListSSet::new(), a.clone(), b.clone());
-    println!("SkipListSSet {} ms", elapsed.as_millis());
+    println!("[{}] SkipListSSet {} ms", label, elapsed.as_millis());
 
     let elapsed = add_remove(Treap::new(), a.clone(), b.clone());
-    println!("Treap {} ms", elapsed.as_millis());
+    println!("[{}] Treap {} ms", label, elapsed.as_millis());
 
-    // std::collections::BTreeSet 41 ms
-    // SkipListSSet 429 ms
-    // Treap 164 ms
+    let elapsed = add_remove(ScapegoatTree::new(), a.clone(), b.clone());
+    println!("[{}] ScapegoatTree {} ms", label, elapsed.as_millis());
+}
+
+fn main() {
+    let mut rng = SmallRng::seed_from_u64(1223334);
+
+    let n = 200_000;
+
+    let mut a = vec![0; n];
+    let mut b = vec![0; n];
+    for i in 0..n {
+        a[i] = rng.gen_range(0..n);
+        b[i] = rng.gen_range(0..n);
+    }
+
+    run("random", a, b);
+    run("sorted", (0..n).collect(), (0..n).collect());
+
+    // [random] std::collections::BTreeSet 45 ms
+    // [random] SkipListSSet 472 ms
+    // [random] Treap 191 ms
+    // [random] ScapegoatTree 176 ms
+
+    // [sorted] std::collections::BTreeSet 34 ms
+    // [sorted] SkipListSSet 301 ms
+    // [sorted] Treap 51 ms
+    // [sorted] ScapegoatTree 608 ms
 }
